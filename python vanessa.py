59519c032f419214c8888277e8e4be8e4530e01e
carrieduranc <126133066+carrieduranc@users.noreply.github.com>
2023-02-22 21:29:08 -0600

    Add files via upload

---
+++ python vanessa.py
@@ -0,0 +1,16 @@
+Usuario="maria"
+Usuario1="jose"
+Contraseña="mari1221"
+Contraseña1="jose56"
+for i in range(1):
+ Usuario=input("ingrese su usuario")
+if usuario==usuario
+ Contraseña=input("ingrese su contraseña")
+ if contraseña==contraseña
+ print("bienvenido")
+ for i in range(1)
+ Usuario1=input("ingrese su usuario1")
+ if usuario1==usuario1
+ Contraseña1=input("ingrese su contraseña1")
+ if contraseña1==contraseña1
+ print("bienvenido")
